dad2c4a13fc89fa90b1d576b6891ec30d19d52ec
Vijendra Achar <44837786+Vijendra-Achar@users.noreply.github.com>
2020-09-22 09:40:42 +0530

    Implemented Security Best Practices🔒🗝

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// Core Module Imports
-
-//
-
-// Framework / Third-Party Module Imports
+// Core / Framework / Third-Party Module Imports
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // Local Module Imports
 const tourRouter = require('./routes/tourRoutes');
@@ -14,20 +15,53 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-// Middleware
+// Rate Limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too Many requests. Please Try again in an Hour.'
+});
+
+// GLOBAL MIDDLEWARES
+
+// MIDDLEWARES / NPM PACKAGES
+// To set Secure HTTP headers
+app.use(helmet());
+
+// To limit the number of requests coming from a single IP
+app.use(limiter);
+
+// Body parser / To read the data from the request Body
 app.use(express.json());
-app.use(morgan('dev'));
 
-/* Custom Middleware */
+// Preventing NOSQL Query Injection
+app.use(mongoSanitize());
 
-// Request property to capture the time of Request.
+// Preventing XSS Attacks
+app.use(xss());
+
+// Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: ['duration', 'price', 'difficulty', 'maxGroupSize', 'ratingsAverage', 'ratingsQuantity']
+  })
+);
+
+// To log all Requests during Development
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// To serve static files
+app.use(express.static(`${__dirname}/public`));
+
+// CUSTOM MIDDLEWARES
+// Middleware to capture the time of Request.
 app.use((req, res, next) => {
   const date = new Date();
   req.time = `Date: ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}, Time: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
   next();
 });
-
-//
 
 /* Router Mounting */
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,21 @@ const signJWTToken = id => {
 const createAndSendToken = (user, statusCode, res) => {
   const myJWTtoken = signJWTToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  // Create and send the JWT via a cookie
+  res.cookie('jwt', myJWTtoken, cookieOptions);
+
+  // Remove the Password from the Response object
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token: myJWTtoken,
