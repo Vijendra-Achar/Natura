29e867e773950e65005f5ba34a2ad8c8557e874b
Vijendra-Achar <vijendra.1995@gmail.com>
2020-11-12 14:44:41 +0530

    Error Handling and Page Implemented ⚠⚒

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,68 +2,96 @@
 const AppError = require('./../utils/appError');
 
 // Function to handle invalid IDs / Cast Error
-const handleCastErrorDB = err => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
 
 // Function to Handle Duplicate entries
-const handleDuplicateError = err => {
+const handleDuplicateError = (err) => {
   const value = err.keyValue.name;
   const message = `The Value : "${value}" already exists. Please find another value. (Duplicate Key Error)`;
   return new AppError(message, 400);
 };
 
 // Function to handle Validation Errors
-const handleValidationErrors = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
+const handleValidationErrors = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid Input Data: ${errors.join(' ')}. (Validation Error)`;
 
   return new AppError(message, 400);
 };
 // Send a detailed report of the error message during Development.
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
+const sendErrorDev = (err, req, res) => {
+  // Response for the API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      errStack: err.stack,
+    });
+  }
+  console.error('ERROR 💥', err);
+  // Response for the Website
+  return res.status(err.statusCode).render('error', {
+    title: 'Oops ⚠',
     message: err.message,
-    error: err,
-    errStack: err.stack
   });
 };
 
 // Send a Simple error message to the client which only contains the message
-const sendErrorProd = (err, res) => {
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
+const sendErrorProd = (err, req, res) => {
+  // Response for the API route
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
     // Log the Error Message to the console during production so that we can see the error message on the hosting platform
     console.error('ERROR 💥', err);
 
     // Send a generic message to the client in case if the error is not of Operational Type
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
-      message: 'Something went Wrong'
+      message: 'Something went Wrong',
     });
   }
+
+  // Response for the Website
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Oops ⚠',
+      message: err.message,
+    });
+  }
+  // Log the Error Message to the console during production so that we can see the error message on the hosting platform
+  console.error('ERROR 💥', err);
+
+  // Send a generic message to the client in case if the error is not of Operational Type
+  return res.status(err.statusCode).render('error', {
+    title: 'Oops ⚠',
+    message: 'Something went Wrong. Please try again later',
+  });
 };
 
 // Invalid JWT Token Error / Mutated payload
-const handleJWTError = err => new AppError('Invalid Token. Please Login again.', 401);
+const handleJWTError = (err) => new AppError('Invalid Token. Please Login again.', 401);
 
 // The JWT Token has Expired Error
-const handleJWTExpiredError = err => new AppError('The Token has Expired. Please Login again', 401);
+const handleJWTExpiredError = (err) => new AppError('The Token has Expired. Please Login again', 401);
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    error.message = err.message;
 
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
@@ -82,6 +110,6 @@ module.exports = (err, req, res, next) => {
     }
 
     // Final Error Message
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const TourModel = require('./../models/tourModels');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/AppError');
 
 // Request Handler for the Home Overview Page
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -15,16 +16,14 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await TourModel.findOne({ slug: req.params.slug });
 
-  res
-    .status(200)
-    // .set(
-    //   'Content-Security-Policy',
-    //   "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;",
-    // )
-    .render('tour', {
-      title: tour.name,
-      tour,
-    });
+  if (!tour) {
+    return next(new AppError('There is no tour by this name', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
 });
 
 // Request handler for the login page
