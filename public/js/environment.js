707775ee1a08276fed3cd1a5be0b9a19171b6563
Vijendra-Achar <vijendra.1995@gmail.com>
2020-12-01 16:44:35 +0530

    Added necessary files ✔

---
+++ public/js/environment.js
@@ -0,0 +1,5 @@
+export const mapboxAccessToken =
+  'pk.eyJ1IjoidmlqZW5kcmFjaGFyIiwiYSI6ImNraDcyaWUxdzBoeDUycG56ano5bHR5Z2kifQ.ccHbOExocP4G_2cJabutbg';
+
+export const stripePublicKey =
+  'pk_test_51HqefwKhpMa58yrgimLc7o65pH7KfPXH6fhgYF2N51cfges4zA9CtmX4Bp8CU68f2ufbtxxpgkDQqIlrF32IEyjl009ILCzKRO';
